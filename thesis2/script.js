66e22ece19a43087acffc17d348422ba97bd37db
edwincomeros <143091489+edwincomeros@users.noreply.github.com>
2025-08-28 20:36:04 -0800

    Delete thesis2 directory
    dwad

--- thesis2/script.js
+++
@@ -1,7 +0,0 @@
-
-setInterval(() => {
-  const now = new Date();
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  document.getElementById('dateTime').innerText = `${now.toLocaleDateString(undefined, options)} | ${time}`;
-}, 1000);
